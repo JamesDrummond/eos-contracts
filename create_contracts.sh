cd3edd794dc1e94208a3bfd4ee3229aed57d2bb0
James Drummond <james@devcomb.com>
2018-01-19 01:02:37 +0000

    Add script to make compiling easier inside build image.
    Signed-off-by: James Drummond <james@devcomb.com>

---
+++ create_contracts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cmake -H. -B"/tmp/build" -GNinja -DCMAKE_BUILD_TYPE=Release -DWASM_LLVM_CONFIG=/opt/wasm/bin/llvm-config -DCMAKE_CXX_COMPILER=clang++ \
+      -DCMAKE_C_COMPILER=clang -DCMAKE_INSTALL_PREFIX=/opt/eos  -DSecp256k1_ROOT_DIR=/usr/local
+cmake --build /tmp/build --target install
