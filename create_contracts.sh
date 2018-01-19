df5ce4cef96dc10e9bbb91fefc59576a0acc1e44
James Drummond <james@devcomb.com>
2018-01-19 01:05:46 +0000

    Signed-off-by: James Drummond <james@devcomb.com>

--- create_contracts.sh
+++ create_contracts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ..
+cd /eos
 cmake -H. -B"/tmp/build" -GNinja -DCMAKE_BUILD_TYPE=Release -DWASM_LLVM_CONFIG=/opt/wasm/bin/llvm-config -DCMAKE_CXX_COMPILER=clang++ \
       -DCMAKE_C_COMPILER=clang -DCMAKE_INSTALL_PREFIX=/opt/eos  -DSecp256k1_ROOT_DIR=/usr/local
 cmake --build /tmp/build --target install
